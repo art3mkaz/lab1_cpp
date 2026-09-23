abb2308a9524a5e669fd7b2a7efd9368a78b9f72
art3mkaz <ap6256360@gmail.com>
2026-09-23 16:25:41 +0300

    Add second output line to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,6 @@
 
 int main() {
     std::cout << "Hello, my name is Zhang A.T. I am from IU5-11/4!" << std::endl;
+    std::cout << "This is the second version of the program." << std::endl;
     return 0;
 }
